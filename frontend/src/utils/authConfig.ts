471410a92075791e7235bc013d7f66a14862bf94
Mishok <mishaonukrnet@ukr.net>
2023-02-23 01:32:10 +0200

    Added access token requesting

--- frontend/src/utils/authConfig.ts
+++ frontend/src/utils/authConfig.ts
@@ -1,4 +1,5 @@
 import {PublicClientApplication} from "@azure/msal-browser";
+import {customSessionStorage} from "./storage";
 
 export const msalConfig = {
     auth: {
@@ -22,9 +23,12 @@ export const msalInstance = new PublicClientApplication(msalConfig);
 
 export const loginHandle = (instance = msalInstance) => {
     instance.loginRedirect(loginRequest).catch(e => console.error(e));
-}
+};
 
 export const logoutHandle = (instance = msalInstance) => {
+    //Remove saved access token
+    customSessionStorage.resetAuthToken();
+
     const currentAccount = msalInstance.getAllAccounts()[0];
     console.log(msalInstance);
     console.log(currentAccount);
@@ -32,4 +36,25 @@ export const logoutHandle = (instance = msalInstance) => {
         account: currentAccount,
         postLogoutRedirectUri: window.location.origin
     }).catch(e => console.error(e))
-}
+};
+
+export const requestAccessToken = async (instance = msalInstance) => {
+    const accounts = msalInstance.getAllAccounts();
+    const name = accounts[0] && accounts[0].name;
+
+    const request = {
+        ...loginRequest,
+        account: accounts[0]
+    };
+
+    return (await instance.acquireTokenSilent(request)).accessToken;
+};
+
+export const getAccessToken = async () => {
+    const token = customSessionStorage.getAuthToken();
+    if(token === null){
+        const token = await requestAccessToken();
+        customSessionStorage.setAuthToken(token);
+    }
+    return token;
+};
